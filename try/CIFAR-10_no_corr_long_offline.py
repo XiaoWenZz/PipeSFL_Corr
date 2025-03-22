08bbb4d767025c6a8bd79f14c2ca20ccb7fd463e
xiaowen <1563687318@qq.com>
2025-03-22 23:08:10 +0800

    debug

--- try/CIFAR-10_no_corr_long_offline.py
+++ try/CIFAR-10_no_corr_long_offline.py
@@ -275,6 +275,8 @@ def train_server(fx_client, y, l_epoch_count, l_epoch, idx, len_batch, net_glob_
             acc_train_collect.append(acc_avg_all_user_train)
             # for debugging print
             print('acc_train_collect appended once, current length:', len(acc_train_collect))
+            print('current idx_collect:', idx_collect)
+            print('current idx_disconnected:', idx_disconnected)
             loss_train_collect.append(loss_avg_all_user_train)
 
     # send gradients to the client
