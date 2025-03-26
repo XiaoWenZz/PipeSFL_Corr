0f4dbfa7ec22601591981b4044d9c26a3615f017
xiaowen <1563687318@qq.com>
2025-03-27 00:15:53 +0800

    fix incorrect corrs

---
+++ try/run_experiments.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python_path="D:/Anaconda3/envs/pytorch/python.exe"
+python_file="CIFAR-10_corr_long_offline_new1.py"
+second_python_file="CIFAR-10_no_corr_long_offline.py"
+
+for epochs in 100; do
+    for disconnect_prob in 0.25 0.20 0.15 0.30 0.35 0.40; do
+        echo "Running with epochs=$epochs and disconnect_prob=$disconnect_prob"
+        $python_path $python_file --epochs $epochs --disconnect_prob $disconnect_prob
+    done
+done
+
+for epochs in 100; do
+    for disconnect_prob in 0.25 0.20 0.15 0.30 0.35 0.40; do
+        echo "Running with epochs=$epochs and disconnect_prob=$disconnect_prob"
+        $python_path $second_python_file --epochs $epochs --disconnect_prob $disconnect_prob
+    done
+done
