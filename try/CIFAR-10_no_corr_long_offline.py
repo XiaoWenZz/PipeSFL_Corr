affcf33851ce72bd334b28649716bab42ca4a25d
xiaowen <1563687318@qq.com>
2025-03-27 16:16:31 +0800

    rename output files

--- try/CIFAR-10_no_corr_long_offline.py
+++ try/CIFAR-10_no_corr_long_offline.py
@@ -944,14 +944,14 @@ if __name__ == '__main__':
     plt.grid(True)
     # 保存图片 按照当前时间保存 目录为 output/curve
     curve_filename = os.path.join(curve_dir,
-                                  f'train_time_curve_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '.png')
+                                  f'train_time_curve_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '.png')
     plt.savefig(curve_filename)
     plt.clf()  # 清除当前图形
 
     # 保存模型 命名为 模型名+当前时间
-    client_model_filename = os.path.join(model_dir, f'Client_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S",
+    client_model_filename = os.path.join(model_dir, f'Client_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S",
                                                                                                          time.localtime()) + '.pth')
-    server_model_filename = os.path.join(model_dir, f'Server_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S",
+    server_model_filename = os.path.join(model_dir, f'Server_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S",
                                                                                                          time.localtime()) + '.pth')
     torch.save(net_glob_client.state_dict(), client_model_filename)
     torch.save(net_glob_server.state_dict(), server_model_filename)
@@ -972,21 +972,21 @@ if __name__ == '__main__':
     loss_test_df = pd.DataFrame(loss_test_collect_list)
 
     # 命名为 模型名+ 数据名+当前时间 目录为 output/acc
-    acc_train_filename = os.path.join(acc_dir, f'Client_Acc_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S",
+    acc_train_filename = os.path.join(acc_dir, f'Client_Acc_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S",
                                                                                                         time.localtime()) + '.csv')
     acc_train_df.to_csv(acc_train_filename, index=False)
     # 命名为 模型名+ 数据名+当前时间 目录为 output/loss
     loss_train_filename = os.path.join(loss_dir,
-                                       f'Client_Loss_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S",
+                                       f'Client_Loss_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S",
                                                                                                  time.localtime()) + '.csv')
     loss_train_df.to_csv(loss_train_filename, index=False)
     # 命名为 模型名+ 数据名+当前时间 目录为 output/acc
-    acc_test_filename = os.path.join(acc_dir, f'Server_Acc_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S",
+    acc_test_filename = os.path.join(acc_dir, f'Server_Acc_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S",
                                                                                                        time.localtime()) + '.csv')
     acc_test_df.to_csv(acc_test_filename, index=False)
     # 命名为 模型名+ 数据名+当前时间 目录为 output/loss
     loss_test_filename = os.path.join(loss_dir,
-                                      f'Server_Loss_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S",
+                                      f'Server_Loss_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S",
                                                                                                 time.localtime()) + '.csv')
     loss_test_df.to_csv(loss_test_filename, index=False)
 
@@ -999,7 +999,7 @@ if __name__ == '__main__':
     plt.legend()
     plt.grid(True)
     acc_curve_filename = os.path.join(curve_dir,
-                                      f'acc_curve_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '.png')
+                                      f'acc_curve_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '.png')
     plt.savefig(acc_curve_filename)
     plt.clf()  # 清除当前图形
 
@@ -1012,7 +1012,7 @@ if __name__ == '__main__':
     plt.legend()
     plt.grid(True)
     loss_curve_filename = os.path.join(curve_dir,
-                                       f'loss_curve_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_' + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '.png')
+                                       f'loss_curve_no_Corr_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_' + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '.png')
     plt.savefig(loss_curve_filename)
     print('Data saved successfully!')
 
