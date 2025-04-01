59a20af64b00145aae171b175739dd41893140bc
xiaowen <1563687318@qq.com>
2025-04-01 15:53:06 +0800

    new gen img script

--- try/output/compare/gen_compare_imgs.py
+++ try/output/compare/gen_compare_imgs.py
@@ -14,11 +14,12 @@ def plot_comparison(no_corr_file, corr_files, output_file):
         y_label = 'Test Acc'
         title_prefix = 'test_acc_ep'
 
-    # 提取 ep, dp, dr, le 值
+    # 提取 ep, dp, dr, le, lr 值
     ep_value = no_corr_file.split('_ep')[1].split('_')[0]
     dp_value = no_corr_file.split('_dp')[1].split('_')[0]
     dr_value = no_corr_file.split('_dr')[1].split('_')[0]
     le_value = no_corr_file.split('_le')[1].split('_')[0] if '_le' in no_corr_file else None
+    lr_value = no_corr_file.split('_lr')[1].split('_')[0] if '_lr' in no_corr_file else None
 
     epochs = range(len(no_corr_data))
 
@@ -32,10 +33,15 @@ def plot_comparison(no_corr_file, corr_files, output_file):
         plt.plot(epochs, corr_data, label=f'Corr (cr={cr_value})')
 
     # 生成标题
-    if le_value:
+    if le_value and lr_value:
+        title = f'{title_prefix}{ep_value}_dp{dp_value}_dr{dr_value}_le{le_value}_lr{lr_value}'
+    elif le_value:
         title = f'{title_prefix}{ep_value}_dp{dp_value}_dr{dr_value}_le{le_value}'
+    elif lr_value:
+        title = f'{title_prefix}{ep_value}_dp{dp_value}_dr{dr_value}_lr{lr_value}'
     else:
         title = f'{title_prefix}{ep_value}_dp{dp_value}_dr{dr_value}'
+
     # 设置图表属性
     plt.xlabel('Epochs')
     plt.ylabel(y_label)
@@ -64,19 +70,31 @@ no_corr_files = [f for f in os.listdir(no_corr_folder) if f.endswith('.csv')]
 for no_corr_file in no_corr_files:
     # 构建 no_corr 文件的完整路径
     no_corr_file_path = os.path.join(no_corr_folder, no_corr_file)
-    # 提取 ep, dp, dr, le 值
+    # 提取 ep, dp, dr, le, lr 值
     ep_value = no_corr_file.split('_ep')[1].split('_')[0]
     dp_value = no_corr_file.split('_dp')[1].split('_')[0]
     dr_value = no_corr_file.split('_dr')[1].split('_')[0]
     le_value = no_corr_file.split('_le')[1].split('_')[0] if '_le' in no_corr_file else None
+    lr_value = no_corr_file.split('_lr')[1].split('_')[0] if '_lr' in no_corr_file else None
     client_or_server = 'Client' if 'Client' in no_corr_file else 'Server'
 
     # 查找对应的 corr 文件
     corr_files = []
-    if le_value:
+    if le_value and lr_value:
+        corr_file_prefix = f'{client_or_server}_Acc_Corr_ep{ep_value}_dp{dp_value}_dr{dr_value}_cr'
+        for f in os.listdir(corr_folder):
+            if f.startswith(corr_file_prefix) and f.endswith(
+                    '.csv') and f'_le{le_value}_' in f and f'_lr{lr_value}_' in f:
+                corr_files.append(os.path.join(corr_folder, f))
+    elif le_value:
         corr_file_prefix = f'{client_or_server}_Acc_Corr_ep{ep_value}_dp{dp_value}_dr{dr_value}_cr'
         for f in os.listdir(corr_folder):
             if f.startswith(corr_file_prefix) and f.endswith('.csv') and f'_le{le_value}_' in f:
+                corr_files.append(os.path.join(corr_folder, f))
+    elif lr_value:
+        corr_file_prefix = f'{client_or_server}_Acc_Corr_ep{ep_value}_dp{dp_value}_dr{dr_value}_cr'
+        for f in os.listdir(corr_folder):
+            if f.startswith(corr_file_prefix) and f.endswith('.csv') and f'_lr{lr_value}_' in f:
                 corr_files.append(os.path.join(corr_folder, f))
     else:
         corr_file_prefix = f'{client_or_server}_Acc_Corr_ep{ep_value}_dp{dp_value}_dr{dr_value}_cr'
@@ -86,13 +104,21 @@ for no_corr_file in no_corr_files:
 
     # 生成标题和文件名
     if client_or_server == 'Client':
-        if le_value:
+        if le_value and lr_value:
+            title = f'train_acc_ep{ep_value}_dp{dp_value}_dr{dr_value}_le{le_value}_lr{lr_value}'
+        elif le_value:
             title = f'train_acc_ep{ep_value}_dp{dp_value}_dr{dr_value}_le{le_value}'
+        elif lr_value:
+            title = f'train_acc_ep{ep_value}_dp{dp_value}_dr{dr_value}_lr{lr_value}'
         else:
             title = f'train_acc_ep{ep_value}_dp{dp_value}_dr{dr_value}'
     else:
-        if le_value:
+        if le_value and lr_value:
+            title = f'test_acc_ep{ep_value}_dp{dp_value}_dr{dr_value}_le{le_value}_lr{lr_value}'
+        elif le_value:
             title = f'test_acc_ep{ep_value}_dp{dp_value}_dr{dr_value}_le{le_value}'
+        elif lr_value:
+            title = f'test_acc_ep{ep_value}_dp{dp_value}_dr{dr_value}_lr{lr_value}'
         else:
             title = f'test_acc_ep{ep_value}_dp{dp_value}_dr{dr_value}'
     output_file = os.path.join(output_folder, f'{title}.png')
