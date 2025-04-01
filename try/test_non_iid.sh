30ad675c80e2f6d672fb4cca7d926895137edc93
xiaowen <1563687318@qq.com>
2025-04-02 01:35:59 +0800

    update fmnist running .sh  script

--- try/test_non_iid.sh
+++ try/test_non_iid.sh
@@ -12,6 +12,8 @@ for disconnect_round in 1; do
                   for lr in 0.0005 0.001 0.003; do
                       echo "Running with epochs=$epochs, disconnect_prob=$disconnect_prob, disconnect_round=$disconnect_round, correction_rate=$correction_rate and local_ep=$local_ep"
                       $python_path $python_file --epochs $epochs --disconnect_prob $disconnect_prob --disconnect_round $disconnect_round --correction_rate $correction_rate --local_ep $local_ep --lr $lr
+                      echo "Running with epochs=$epochs, disconnect_prob=$disconnect_prob, disconnect_round=$disconnect_round and local_ep=$local_ep"
+                  $python_path $second_python_file --epochs $epochs --disconnect_prob $disconnect_prob --disconnect_round $disconnect_round --local_ep $local_ep --lr $lr
                   done
                 done
             done
@@ -19,15 +21,15 @@ for disconnect_round in 1; do
     done
 done
 
-for disconnect_round in 1; do
-    for epochs in 20; do
-        for disconnect_prob in 0.50 0.60; do
-            for local_ep in 5; do
-              for lr in 0.0005 0.001 0.003; do
-                  echo "Running with epochs=$epochs, disconnect_prob=$disconnect_prob, disconnect_round=$disconnect_round and local_ep=$local_ep"
-                  $python_path $second_python_file --epochs $epochs --disconnect_prob $disconnect_prob --disconnect_round $disconnect_round --local_ep $local_ep --lr $lr
-              done
-            done
-        done
-    done
-done
+#for disconnect_round in 1; do
+#    for epochs in 20; do
+#        for disconnect_prob in 0.40 0.50 0.60; do
+#            for local_ep in 5; do
+#              for lr in 0.0005 0.001 0.003; do
+#                  echo "Running with epochs=$epochs, disconnect_prob=$disconnect_prob, disconnect_round=$disconnect_round and local_ep=$local_ep"
+#                  $python_path $second_python_file --epochs $epochs --disconnect_prob $disconnect_prob --disconnect_round $disconnect_round --local_ep $local_ep --lr $lr
+#              done
+#            done
+#        done
+#    done
+#done
