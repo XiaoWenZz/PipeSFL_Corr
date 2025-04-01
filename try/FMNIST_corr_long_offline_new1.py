f1d8a48d68370dd74fd33c340c4f927f31c512bc
xiaowen <1563687318@qq.com>
2025-04-02 01:30:02 +0800

    update fmnist running .sh  script

--- try/FMNIST_corr_long_offline_new1.py
+++ try/FMNIST_corr_long_offline_new1.py
@@ -1130,7 +1130,7 @@ if __name__ == '__main__':
     plt.ylabel('Training Time (s)')
     plt.title('Training Time Curve')
     plt.grid(True)
-    prefix = f"_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_cr{args.correction_rate:.2f}_le{args.local_ep}_lr{args.lr}"
+    prefix = f"_FMNIT_ep{args.epochs}_dp{args.disconnect_prob:.2f}_dr{args.disconnect_round}_cr{args.correction_rate:.2f}_le{args.local_ep}_lr{args.lr}"
     # 保存图片 按照当前时间保存 目录为 output/curve
     curve_filename = os.path.join(curve_dir, f'train_time_curve{prefix}_' + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '.png')
     plt.savefig(curve_filename)
