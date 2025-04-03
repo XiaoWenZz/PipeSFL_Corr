38c120bcf426007e6611b6ad80efc2f4c64e7555
XiaoWenZz <1563687318@qq.com>
2025-04-04 01:17:28 +0800

    sync

---
+++ try/test_epochs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+python_path="D:/Anaconda3/envs/pytorch/python.exe"
+python_file="FMNIST_corr_long_offline_new1.py"
+second_python_file="FMNIST_no_corr_long_offline.py"
+
+for disconnect_round in 1; do
+    for epochs in 40; do
+        for disconnect_prob in 0.30 0.50 0.60 0.70; do
+            for correction_rate in 1.0; do
+                for local_ep in 5; do
+                  for lr in 0.001 0.003; do
+                      echo "Running with epochs=$epochs, disconnect_prob=$disconnect_prob, disconnect_round=$disconnect_round, correction_rate=$correction_rate and local_ep=$local_ep"
+                      $python_path $python_file --epochs $epochs --disconnect_prob $disconnect_prob --disconnect_round $disconnect_round --correction_rate $correction_rate --local_ep $local_ep --lr $lr
+                      echo "Running with epochs=$epochs, disconnect_prob=$disconnect_prob, disconnect_round=$disconnect_round and local_ep=$local_ep"
+                      $python_path $second_python_file --epochs $epochs --disconnect_prob $disconnect_prob --disconnect_round $disconnect_round --local_ep $local_ep --lr $lr
+                  done
+                done
+            done
+        done
+    done
+done
