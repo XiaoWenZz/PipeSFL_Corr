ba2b2f415cf6f528ef08c5d67ced23bc1653f058
XiaoWenZz <1563687318@qq.com>
2025-04-06 13:59:55 +0800

    sync

--- try/test_non_iid_cifar.sh
+++ try/test_non_iid_cifar.sh
@@ -5,11 +5,11 @@ python_file="CIFAR_corr_long_offline_new2.py"
 second_python_file="CIFAR_no_corr_long_offline.py"
 
 for disconnect_round in 1; do
-    for epochs in 20 40; do
-        for disconnect_prob in 0.40 0.50 0.60; do
-            for correction_rate in 1.0; do
-                for local_ep in 5 10; do
-                  for lr in 0.001 0.003 0.01; do
+    for epochs in 20; do
+        for disconnect_prob in 0.40; do
+            for correction_rate in 0.9 1.0; do
+                for local_ep in 5; do
+                  for lr in 0.0001; do
                       echo "Running with epochs=$epochs, disconnect_prob=$disconnect_prob, disconnect_round=$disconnect_round, correction_rate=$correction_rate and local_ep=$local_ep"
                       $python_path $python_file --epochs $epochs --disconnect_prob $disconnect_prob --disconnect_round $disconnect_round --correction_rate $correction_rate --local_ep $local_ep --lr $lr
                       echo "Running with epochs=$epochs, disconnect_prob=$disconnect_prob, disconnect_round=$disconnect_round and local_ep=$local_ep"
